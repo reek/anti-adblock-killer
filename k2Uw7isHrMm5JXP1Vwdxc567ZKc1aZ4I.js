a0c1f7b1c1c2f1d4cf7a526199da0c1efde87339
reek <reekside@gmail.com>
2016-06-02 05:34:44 +0200

    update

--- k2Uw7isHrMm5JXP1Vwdxc567ZKc1aZ4I.js
+++ k2Uw7isHrMm5JXP1Vwdxc567ZKc1aZ4I.js
@@ -1,1 +1,7 @@
-document.write('<div id="k2Uw7isHrMm5JXP1Vwdxc567ZKc1aZ4I" style="display:none;">(^_^)</div>');
+(function () {
+  var el = document.createElement("div");
+  el.innerHTML = "(^_^)";
+  el.style.display = 'none';
+  el.id = 'k2Uw7isHrMm5JXP1Vwdxc567ZKc1aZ4I';
+  document.body.appendChild(el);
+})()
